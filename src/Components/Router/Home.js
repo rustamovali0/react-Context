6b7f0ae1489ed24fb0c65cb10c1e2fb0609e6bac
rustamovali0 <rstmv200@gmail.com>
2023-11-20 12:16:52 +0400

    useReducer()

--- src/Components/Router/Home.js
+++ src/Components/Router/Home.js
@@ -1,35 +1,52 @@
 // Home.js
-import React from "react";
-import { useContext } from "react";
+import React, { useReducer, useEffect, useContext } from "react";
 import { Context } from '../../Context/Context';
 
+const initialState = {
+  users: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'setUser':
+      return { ...state, users: action.setUsers };
+    default:
+      return state;
+  }
+};
 
 const Home = () => {
-  const [users, setUser] = React.useState([])
-  const { theme, setTheme } = useContext(Context)
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { users } = state;
+
+  const { theme, setTheme } = useContext(Context);
   const { textColor, setTextColor } = useContext(Context)
 
-  const requestEmpolyee = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    if (res.ok && res.status === 200) {
-      const data = await res.json()
-      setUser(data)
-    } else {
-      alert("An error occurred while fetching data.")
+  const requestEmployee = async () => {
+    try {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users');
+      if (res.ok && res.status === 200) {
+        const data = await res.json();
+        dispatch({ type: 'setUser', setUsers: data });
+      } else {
+        alert("An error occurred while fetching data.");
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-  }
-  
-  React.useEffect(() => {
-    requestEmpolyee()
-  }, [])
+  };
+
+  useEffect(() => {
+    requestEmployee();
+  }, []);
 
   return (
-    <div className={`bg-${theme} text-${textColor}`} style={{width:"100%", height:"94vh"}}>
+    <div className={`bg-${theme} text-${textColor}`} style={{ width: "100%", height: "94vh" }}>
       {users.map((item, index) => (
         <h1 key={index}>{item.name}</h1>
       ))}
     </div>
-  )
+  );
 };
 
 export default Home;
